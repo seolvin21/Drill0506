3e33677204eea52a7016f7505f34579a0b364704
seolvin21 <129517269+seolvin21@users.noreply.github.com>
2023-10-04 11:42:26 +0900

    completed moving the character to a random hand arrow location that is created repeatedly if t > 1, create a new hand arrow at a random loaction

--- drill05.py
+++ drill05.py
@@ -36,6 +36,11 @@ def reset_world():
     frame = 0
     action = 3
 
+    set_new_target_arrow()
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
     sx, sy = cx, cy  # p1: 시작점
     # hx, hy = 50, 50
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2: 끝점
@@ -63,6 +68,9 @@ def update_world():
         cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
         cy = (1 - t) * sy + t * hy
         t += 0.001
+    else:
+        cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 정확히 일치시킴
+        set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
