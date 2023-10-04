2e49d33e18cf8fb62e0e25e4941c6f150e28be76
seolvin21 <129517269+seolvin21@users.noreply.github.com>
2023-10-04 12:28:32 +0900

    Completed characters moving through the target points

--- drill06.py
+++ drill06.py
@@ -41,21 +41,27 @@ def reset_world():
     action = 3
 
     points = [(100, 900), (1200, 800), (500, 100)]
-    # set_new_target_arrow()
+    set_new_target_arrow()
 
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
+    global target_exists
 
-    sx, sy = cx, cy  # p1: 시작점
-    # hx, hy = 50, 50
-    hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2: 끝점
-    t = 0.0
-    action = 1 if sx < hx else 0
-    frame = 0
-
+    if points:     # points 리스트 안에 남아있는 점이 있으면
+        sx, sy = cx, cy  # p1: 시작점
+        # hx, hy = 50, 50
+        hx, hy = points[0]  # p2: 끝점
+        t = 0.0
+        action = 1 if sx < hx else 0
+        frame = 0
+        target_exists =True
+    else:
+        action = 3 if action==1 else 2   # 이전에 소년이 우측으로 이동 중이었으면, IDLE 동작시 우측을 바라보도록
+        frame = 0
+        target_exists = False
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
@@ -73,13 +79,15 @@ def update_world():
 
     frame = (frame + 1) % 8
 
-    # if t <= 1.0:
-    #     cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
-    #     cy = (1 - t) * sy + t * hy
-    #     t += 0.001
-    # else:
-    #     cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 정확히 일치시킴
-    #     set_new_target_arrow()
+    if target_exists:
+        if t <= 1.0:
+            cx = (1 - t) * sx + t * hx  # cx는 시작 x 와 끝 x 를 1-t:t 의 비율로 섞은 위치
+            cy = (1 - t) * sy + t * hy
+            t += 0.001
+        else:       # 목표 지점에 도달하면
+            cx, cy = hx, hy  # 캐릭터 위치를 목적지 위치와 강제로 정확히 일치시킴
+            del points[0]   # 목표 지점에 왔기 때문에 더이상 필요없는 점을 삭제
+            set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
